23b1d33f40f8d99fe267088ba111677ab69cb226
Abubaker <abubakershekhani@gmail.com>
2021-01-08 12:50:17 +0500

    add location pages. Sprint 1

---
+++ src/pages/location/city3.js
@@ -0,0 +1,28 @@
+import * as React from "react"
+
+
+const pageStyles = {
+    color: "#232129",
+    padding: "96px",
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  }
+
+  const headingStyles = {
+    marginTop: 0,
+    marginBottom: 64,
+    maxWidth: 320,
+  }
+
+
+  const City3Page = () => {
+    return (
+        <main style={pageStyles}>
+            <title>City Page</title>
+            <h1 style={headingStyles}>
+                City 3
+            </h1>
+        </main>
+    )
+  }
+
+  export default City3Page
